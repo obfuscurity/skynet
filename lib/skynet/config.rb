9671a288ccd4838a3b8140fa2b93fa7ef251df3b
obfuscurity <jason@dixongroup.net>
2012-06-10 04:19:16 -0400

    add configs for scaling url and redistogo

--- lib/skynet/config.rb
+++ lib/skynet/config.rb
@@ -5,6 +5,8 @@ module Skynet
     end
 
     def self.port; env!("PORT").to_i; end
+    def self.heroku_api_scaling_url; env!("HEROKU_API_SCALING_URL"); end
     def self.rack_env; env!("RACK_ENV"); end
+    def self.redistogo_url; env!("REDISTOGO_URL"); end
   end
 end
